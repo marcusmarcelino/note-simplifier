aa88d78b211283b5e061581af94bd33cf44d6a59
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2021-12-01 00:11:21 -0300

    authentication with token generation and refresh completed

--- server-note-simplifier/src/main/java/br/com/apinotesimplifier/controllers/UserController.java
+++ server-note-simplifier/src/main/java/br/com/apinotesimplifier/controllers/UserController.java
@@ -1,7 +1,23 @@
 package br.com.apinotesimplifier.controllers;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,37 +25,80 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.apinotesimplifier.interfaces.UserAndPersonalData;
 import br.com.apinotesimplifier.interfaces.UserService;
-import br.com.apinotesimplifier.models.PersonalData;
+import br.com.apinotesimplifier.models.Role;
 import br.com.apinotesimplifier.models.User;
 import lombok.Data;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class UserController {
   @Autowired
   private UserService userService;
 
-  @GetMapping("/all")
+  private String AUTHORIZATION = "Authorization";
+  private String APPLICATION_JSON_VALUE = "application/json";
+
+  @GetMapping("/users/all")
   public ResponseEntity<List<User>> getUsers() {
     return ResponseEntity.ok().body(this.userService.getUsers());
   }
 
-  @PostMapping("/save")
+  @PostMapping("/users/save")
   public ResponseEntity<User> saveUser(@RequestBody UserAndPersonalData userAndPersonalData) {
     URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/save").toUriString());
     return ResponseEntity.created(uri).body(this.userService.saveUser(userAndPersonalData));
   }
 
-  @PostMapping("/addtouser")
+  @PostMapping("/users/addtouser")
   public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
     this.userService.addRoleToUser(form.getUsername(), form.getRolename());
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/token/refresh")
+  public void refreshToken(HttpServletRequest request, HttpServletResponse response)
+      throws StreamWriteException, DatabindException, IOException {
+    String authorizationHeader = request.getHeader(AUTHORIZATION);
+
+    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+      try {
+        String refresh_token = authorizationHeader.substring("Bearer ".length());
+
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT decodedJWT = verifier.verify(refresh_token);
+        String username = decodedJWT.getSubject();
+        User user = this.userService.getUser(username);
+
+        String access_token = JWT.create()
+            .withSubject(user.getUsername())
+            .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 1200 * 1000))
+            .withIssuer(request.getRequestURL().toString())
+            .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
+            .sign(algorithm);
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+      } catch (Exception e) {
+        response.setHeader("error", e.getMessage());
+        response.setStatus(403);
+        Map<String, String> error = new HashMap<>();
+        error.put("error_message", e.getMessage());
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), error);
+      }
+    } else {
+      throw new RuntimeException("Refresh token is missing");
+    }
   }
 }
 
--- server-note-simplifier/src/main/java/br/com/apinotesimplifier/security/SecurityConfig.java
+++ server-note-simplifier/src/main/java/br/com/apinotesimplifier/security/SecurityConfig.java
@@ -11,8 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.com.apinotesimplifier.filter.CustomAuthenticationFilter;
+import br.com.apinotesimplifier.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -29,11 +31,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+    customAuthenticationFilter.setFilterProcessesUrl("/api/login");
     http.csrf().disable();
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.authorizeHttpRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
     http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**").hasAnyAuthority("ROLE_USER");
-    http.authorizeHttpRequests().anyRequest().permitAll();
-    http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+    http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/users/save/**").hasAnyAuthority("ROLE_ADMIN");
+    http.authorizeHttpRequests().anyRequest().authenticated();
+    http.addFilter(customAuthenticationFilter);
+    http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
   }
 
   @Bean
