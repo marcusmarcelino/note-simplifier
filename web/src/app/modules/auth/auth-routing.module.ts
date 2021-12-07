7a7208aaaafbc78d2efb850c534a26ee6dfd0066
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2021-12-07 13:57:24 -0300

    refactoring in the login screen flow

--- web/src/app/modules/auth/auth-routing.module.ts
+++ web/src/app/modules/auth/auth-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LoginComponent,
-    children: [
-      { path: 'signin', component: SigninComponent },
-      { path: 'signup', component: SignupComponent },
-    ]
-  }
+  { path: '', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
