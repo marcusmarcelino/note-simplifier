27af82e84aaf62527779ea087541e891cc9cad9c
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2022-06-18 17:21:25 -0300

    tweaks to methods declared in repository

--- server-note-simplifier/src/main/java/br/com/apinotesimplifier/repository/ServiceProvidedRepository.java
+++ server-note-simplifier/src/main/java/br/com/apinotesimplifier/repository/ServiceProvidedRepository.java
@@ -1,19 +1,15 @@
 package br.com.apinotesimplifier.repository;
 
 import java.time.LocalDate;
-// import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
 
 import br.com.apinotesimplifier.models.ServiceProvided;
 import br.com.apinotesimplifier.models.User;
 
 public interface ServiceProvidedRepository extends JpaRepository<ServiceProvided, Long> {
-  // @Query("SELECT serviceProvided FROM ServiceProvided serviceProvided WHERE
-  // serviceProvided.idClient.id = :idClient ")
   Page<ServiceProvided> findByIdClient(User idClient, Pageable pageable);
 
   Page<ServiceProvided> findByIdProfessional(User idProfessional, Pageable pageable);
